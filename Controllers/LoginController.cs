ba0e557db4e046a4289835556014122062d3b277
htilssu <tolashuu@gmail.com>
2023-09-07 20:57:58 +0700

    Test merge

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shoppe_Clone.Controllers;
+
+public class LoginController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
