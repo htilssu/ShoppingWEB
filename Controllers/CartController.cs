51f2d9c31dfd6efe594a40fcd3e1583ae5bfcbad
htilssu <tolashuu@gmail.com>
2023-09-15 09:17:51 +0700

    Add cart controller

---
+++ Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingWEB.Controllers;
+
+public class CartController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
