6b711cffce9cad9e33e3a924d565cca6b52c0588
htilssu <tolashuu@gmail.com>
2023-09-20 09:50:50 +0700

    Update: updated cookie expired

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,4 +10,9 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    public IActionResult AccessDeny()
+    {
+        return View();
+    }
 }
--- Program.cs
+++ Program.cs
@@ -19,6 +19,24 @@ builder.Services.AddIdentity<UserModel, RoleModel>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 0;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Login";
+    options.LogoutPath = "/";
+    options.Cookie.HttpOnly = true;
+    options.Cookie.MaxAge = TimeSpan.FromHours(6);
+});
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
