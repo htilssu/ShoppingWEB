b13be48ee8b967bb27d714db0c750238c9f78a00
htilssu <tolashuu@gmail.com>
2023-10-01 02:10:25 +0700

    Add: added fully product,category controller

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingWEB.Models;
+
+public class Category
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public string? CategoryName { get; set; }
+
+    public string? ImagePath { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? UpdatedBy { get; set; }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace ShoppingWEB.Models;
+
+public class Product
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public string? ProductName { get; set; }
+
+    public double? Price { get; set; }
+
+    public double? DiscountPrice { get; set; }
+
+    public int? InStock { get; set; }
+
+    public string? ShortDescription { get; set; }
+
+    public string? ProductDescription { get; set; }
+
+    public sbyte? Published { get; set; }
+
+    public string? CategoryId { get; set; }
+
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
