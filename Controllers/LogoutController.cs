49eb4b4d50cba5a1f092868cf1418b604d1d450f
htilssu <tolashuu@gmail.com>
2023-10-01 15:40:38 +0700

    Add: added logout controller

---
+++ Controllers/LogoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingWEB.Models;
+
+namespace ShoppingWEB.Controllers;
+
+public class LogoutController : Controller
+{
+    private readonly SignInManager<UserModel> _signInManager;
+    private UserManager<UserModel> _userManager;
+
+    public LogoutController(UserManager<UserModel> userManager, SignInManager<UserModel> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
