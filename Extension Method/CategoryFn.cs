380848a479c87ab436678083533e57145b618c47
htilssu <tolashuu@gmail.com>
2023-10-08 21:04:21 +0700

    aa

---
+++ Extension Method/CategoryFn.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingWEB.Models;
+
+namespace ShoppingWEB.Extension_Method;
+
+public static class CategoryFn
+{
+    public static string GetWebPath(this Category category)
+    {
+        var path = category.ImagePath;
+        return path![path.IndexOf('\\')..].Replace(@"\", "/");
+    }
+}
