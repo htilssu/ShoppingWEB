5f4b0e9d93418512a7a178a939a8f1d0757a4793
htilssu <tolashuu@gmail.com>
2023-10-11 18:24:23 +0700

    Add: added product detail

---
+++ Controllers/DetailController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingWEB.Controllers;
+
+public class DetailController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public DetailController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    // GET
+    public async Task<IActionResult> Index(string id)
+    {
+        var product = await _db.Products.FindAsync(id);
+        // if (product is null) return RedirectToAction("Index", "Home");
+        return View();
+    }
+}
