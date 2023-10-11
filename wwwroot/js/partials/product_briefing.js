fb7c7985b469ef5bb34af0cedc1363cb4e60720e
htilssu <tolashuu@gmail.com>
2023-10-12 00:25:20 +0700

    Add: added script ProductBriefing

---
+++ wwwroot/js/partials/product_briefing.js
@@ -0,0 +1,33 @@
+﻿const subImageCol = $(".sub-image-item");
+subImageCol.on('mouseover', changePreviewImage)
+
+let lastImage = null
+let firstSubImage = subImageCol.first();
+changeLastImage(firstSubImage)
+
+function changeLastImage(e) {
+    if (lastImage === firstSubImage) {
+        lastImage.removeClass('viewing');
+    } else if (lastImage !== null) {
+        lastImage.classList.remove("viewing")
+    }
+
+    if (e === firstSubImage) {
+        e.addClass("viewing")
+        lastImage = e;
+    } else {
+        e.closest('.sub-image-item').classList.add("viewing")
+        lastImage = e.closest('.sub-image-item');
+    }
+}
+
+
+function changePreviewImage(e) {
+    let attr = e.target.getAttribute('src');
+    if (attr !== null && attr !== "") {
+        changeLastImage(e.target)
+        $("#product-img-preview").attr({
+            src: e.target.getAttribute('src')
+        })
+    }
+}
