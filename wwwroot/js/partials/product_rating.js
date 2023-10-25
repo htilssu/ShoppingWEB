c25c3e52c49eab287c2e433e204b20fbd73afd72
htilssu <tolashuu@gmail.com>
2023-10-25 10:03:58 +0700

    Add: product-rating interact

---
+++ wwwroot/js/partials/product_rating.js
@@ -0,0 +1,13 @@
+﻿const ratingFilters = $(".product-rating-filter__item")
+
+ratingFilters.first().addClass("viewing")
+
+function handleFilterChange(e) {
+    ratingFilters.each(function (index, element) {
+        if ($(element).hasClass("viewing")) $(element).removeClass("viewing")
+    })
+
+    $(this).addClass("viewing")
+}
+
+ratingFilters.on("click", handleFilterChange)
