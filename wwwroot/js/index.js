cbbf878ed4e3ddd50343ba82d9c7235da0781bd0
htilssu <tolashuu@gmail.com>
2023-10-26 22:58:24 +0700

    Update: add user comment

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,13 +1,13 @@
 ﻿//slide category to right
 $(".next-item-btn-right").on("click", () => {
-    $(".item-list").css({
-        transform: 'translateX(-21%)'
-    })
-})
+  $(".item-list").css({
+    transform: `translateX(calc(-144px * 1))`,
+  });
+});
 
 //slide category to left
 $(".next-item-btn-left").on("click", () => {
-    $(".item-list").css({
-        transform: 'translateX(0)'
-    })
-})
+  $(".item-list").css({
+    transform: "translateX(0)",
+  });
+});
