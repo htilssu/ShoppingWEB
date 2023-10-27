bb02a51e62444b8e7eb0a7c6cc9906dc8544d0ff
titvvn <tuanmeo980provip@gmail.com>
2023-10-27 18:39:43 +0700

    update gitignore

---
+++ wwwroot/js/partials/product_rating.js
@@ -0,0 +1,18 @@
+﻿const ratingFilters = $(".product-rating-filter__item");
+const previewSubImage = $(".posted-content__sub-image img");
+
+ratingFilters.on("click", handleFilterChange);
+
+previewSubImage.on("click", handleChangePreview);
+
+ratingFilters.first().addClass("viewing");
+
+function handleFilterChange(e) {
+  ratingFilters.each(function (index, element) {
+    if ($(element).hasClass("viewing")) $(element).removeClass("viewing");
+  });
+
+  $(this).addClass("viewing");
+}
+
+function handleChangePreview(e) {}
