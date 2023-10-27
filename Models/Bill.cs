36e9b9b3e9ea42ce009c84b3bfb66855db038c0e
htilssu <tolashuu@gmail.com>
2023-10-27 21:33:06 +0700

    Add: Bill model

---
+++ Models/Bill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingWEB.Models;
+
+public partial class Bill
+{
+    public string Id { get; set; } = null!;
+
+    public string ItemId { get; set; } = null!;
+
+    public double Total { get; set; }
+
+    public virtual CartItem Item { get; set; } = null!;
+}
