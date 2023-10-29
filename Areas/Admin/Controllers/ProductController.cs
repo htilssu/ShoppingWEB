eead03b58d9c0da00e4af4233560b4eb91733343
htilssu <tolashuu@gmail.com>
2023-10-29 18:04:08 +0700

    Add: product page

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Org.BouncyCastle.Tsp;
+using Org.BouncyCastle.Utilities.IO.Pem;
+using ShoppingWEB.Areas.Admin.Models;
+using ShoppingWEB.Extension_Method;
+using ShoppingWEB.Models;
+
+
+namespace ShoppingWEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ShoppingContext _context;
+
+        public ProductController(ShoppingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string? categoryId)
+        {
+            if (!string.IsNullOrEmpty(categoryId))
+            {
+                var categoryProduct =
+                    await _context.Products
+                        .Where(product => product.CategoryId == categoryId)
+                        .ToListAsync();
+                return View(categoryProduct);
+            }
+
+            var productList = await _context.Products.ToListAsync();
+            return View(productList);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OnPostCreate(ProductModel product)
+        {
+            if (!ModelState.IsValid) return RedirectToAction("Index", "Product");
+            try
+            {
+                var pathImage = await product.ImageFile.SaveImage();
+                product.ImageUrls.Add(new ImageUrl()
+                {
+                    Product = product,
+                    ImagePath = pathImage,
+                    ProductId = product.Id,
+                });
+                await _context.Products.AddAsync(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
