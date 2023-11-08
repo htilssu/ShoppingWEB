e2a2a0302e22f18d598626df48870786c25d7cd1
htilssu <tolashuu@gmail.com>
2023-11-09 00:18:28 +0700

    add comment model

---
+++ Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingWEB.Models;
+
+public partial class Comment
+{
+    public string Id { get; set; } = null!;
+
+    public string? Content { get; set; }
+
+    public string? UserId { get; set; }
+
+    public DateTime? Time { get; set; }
+
+    public string? TypeProductId { get; set; }
+
+    public string? Material { get; set; }
+
+    public string? Color { get; set; }
+
+    public string? LikeDetail { get; set; }
+    public UserModel? User { get; set; }
+    public TypeProduct? TypeProduct { get; set; }
+}
