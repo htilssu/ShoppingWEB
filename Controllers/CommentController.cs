f8a1bd815411d763edb60f2192abfd119e0fbf0c
toan <toandong2004@gmail.com>
2023-11-14 23:35:55 +0700

    update

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingWEB.Controllers;
+
+public class CommentController : Controller
+{
+    
+    private ShoppingContext _context;
+
+    public CommentController(ShoppingContext context)
+    {
+        _context=context;
+    }
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
