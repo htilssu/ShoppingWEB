3a5466c45b1fe3dfeba6994398af82fe2ffd415b
toan <toandong2004@gmail.com>
2023-11-14 14:09:00 +0700

    fix

--- wwwroot/js/Actions/jscate.js
+++ wwwroot/js/Actions/jscate.js
@@ -1,23 +1,25 @@
-﻿const clearAllButton = document.getElementById('clearAll');
-const checkboxInputs = document.querySelectorAll('.category-group-item-check');
-var priceFromInput = document.getElementById("priceFrom");
-var priceToInput = document.getElementById("priceTo");
+﻿const clearAllButton = document.getElementById("clearAll");
+const checkboxInputs = document.querySelectorAll(".category-group-item-check");
+var priceFromInput = document.getElementById("PriceFrom");
+var priceToInput = document.getElementById("PriceTo");
 var applyFilterButton = document.getElementById("applyFilter");
 
-// Hàm áp dụng bộ lọc
-function applyPriceFilter() {
+clearAllButton.addEventListener("click", clearAll);
+// Xử lý sự kiện khi nút "Áp dụng" được nhấn
+applyFilterButton.addEventListener("click", function () {
     var priceFromValue = parseFloat(priceFromInput.value);
     var priceToValue = parseFloat(priceToInput.value);
 
-    if (isNaN(priceFromValue) || isNaN(priceToValue) || priceFromValue > priceToValue) {
-        // Xử lý khi giá trị không hợp lệ
+    if (
+        isNaN(priceFromValue) ||
+        isNaN(priceToValue) ||
+        priceFromValue > priceToValue
+    ) {
     } else {
         // Thực hiện logic áp dụng bộ lọc ở đây
         console.log("Giá từ: " + priceFromValue + " đến: " + priceToValue);
-
-        // Đặt các logic áp dụng bộ lọc khác ở đây
     }
-}
+});
 
 // Kiểm tra giá trị nhập liệu để ngăn người dùng nhập các ký tự không phải số
 priceFromInput.addEventListener("input", function () {
@@ -28,21 +30,18 @@ priceToInput.addEventListener("input", function () {
     priceToInput.value = priceToInput.value.replace(/[^0-9]/g, "");
 });
 
-// Xử lý sự kiện khi nút "Áp dụng" được nhấn
-applyFilterButton.addEventListener("click", applyPriceFilter);
-
-// Hàm xóa tất cả các checkbox đã chọn và áp dụng bộ lọc
+// Hàm xóa tất cả các checkbox đã chọn
 function clearAll() {
-    checkboxInputs.forEach(input => {
+    checkboxInputs.forEach((input) => {
         input.checked = false;
     });
-
-    // Xóa giá trị của ô nhập priceFrom và priceTo
-    priceFromInput.value = "";
-    priceToInput.value = "";
-
-    // Áp dụng bộ lọc sau khi xóa tất cả checkbox và giá trị ô nhập
-    applyPriceFilter();
+    // Thực hiện logic sau khi xóa tất cả checkbox ở đây
 }
 
-clearAllButton.addEventListener('click', clearAll);
+$(".category-group-item-check").on("change", function () {
+    // Nếu checkbox này được chọn, hủy chọn tất cả các checkbox khác
+    if ($(this).prop("checked")) {
+        console.log(1);
+        $(".category-group-item-check").not(this).prop("checked", false);
+    }
+});
