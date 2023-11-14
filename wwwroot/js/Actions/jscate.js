11d8ef1aa18a7c911065966eb6f10ced58be18ea
toan <toandong2004@gmail.com>
2023-11-14 10:38:18 +0700

    a

--- wwwroot/js/Actions/jscate.js
+++ wwwroot/js/Actions/jscate.js
@@ -3,19 +3,21 @@ const checkboxInputs = document.querySelectorAll('.category-group-item-check');
 var priceFromInput = document.getElementById("priceFrom");
 var priceToInput = document.getElementById("priceTo");
 var applyFilterButton = document.getElementById("applyFilter");
-clearAllButton.addEventListener('click', clearAll);
-// Xử lý sự kiện khi nút "Áp dụng" được nhấn
-applyFilterButton.addEventListener("click", function () {
+
+// Hàm áp dụng bộ lọc
+function applyPriceFilter() {
     var priceFromValue = parseFloat(priceFromInput.value);
     var priceToValue = parseFloat(priceToInput.value);
 
     if (isNaN(priceFromValue) || isNaN(priceToValue) || priceFromValue > priceToValue) {
-
+        // Xử lý khi giá trị không hợp lệ
     } else {
         // Thực hiện logic áp dụng bộ lọc ở đây
         console.log("Giá từ: " + priceFromValue + " đến: " + priceToValue);
+
+        // Đặt các logic áp dụng bộ lọc khác ở đây
     }
-});
+}
 
 // Kiểm tra giá trị nhập liệu để ngăn người dùng nhập các ký tự không phải số
 priceFromInput.addEventListener("input", function () {
@@ -26,13 +28,21 @@ priceToInput.addEventListener("input", function () {
     priceToInput.value = priceToInput.value.replace(/[^0-9]/g, "");
 });
 
-applyFilterButton.addEventListener('click', applyPriceFilter);
+// Xử lý sự kiện khi nút "Áp dụng" được nhấn
+applyFilterButton.addEventListener("click", applyPriceFilter);
 
-
-// Hàm xóa tất cả các checkbox đã chọn
+// Hàm xóa tất cả các checkbox đã chọn và áp dụng bộ lọc
 function clearAll() {
     checkboxInputs.forEach(input => {
         input.checked = false;
     });
-    // Thực hiện logic sau khi xóa tất cả checkbox ở đây
+
+    // Xóa giá trị của ô nhập priceFrom và priceTo
+    priceFromInput.value = "";
+    priceToInput.value = "";
+
+    // Áp dụng bộ lọc sau khi xóa tất cả checkbox và giá trị ô nhập
+    applyPriceFilter();
 }
+
+clearAllButton.addEventListener('click', clearAll);
